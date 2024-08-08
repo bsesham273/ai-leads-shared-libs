f260605ac97c56a207bfa33eb1b7d2bd19de7a30
bsesham273 <bsesham273@gmail.com>
2024-08-08 11:30:47 +0530

    Update nexusUpload.groovy

--- nexusUpload.groovy
+++ nexusUpload.groovy
@@ -1,6 +1,6 @@
 def call(repoName,artifactId,groupId){
- def pom = readMavenPom file: 'pom.xml'
-               def version = pom.version
+    def pom = readMavenPom file: 'pom.xml'
+    def version = pom.version
                 nexusArtifactUploader artifacts: [[artifactId: artifactId, classifier: '', file: "target/${artifactId}.war", type: 'war']], 
                 credentialsId: 'nexus', 
                 groupId: groupId, 
